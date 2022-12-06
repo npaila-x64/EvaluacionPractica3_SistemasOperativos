3137fabd87a7c8a38db1d7db7889c6c54e2edb25
N. Paila <n.paila01@ufromail.cl>
2022-12-06 15:50:50 -0300

    implementa funcionalidad básica de cliente-servidor

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,7 +1,16 @@
-public class Cliente {
+import java.net.Socket;
 
+public class Cliente {
     public static void main(String[] args) {
-        
+        new Cliente().iniciar();
     }
 
+    private void iniciar() {
+        try (Socket socket = new Socket("localhost", 7287);) {
+            System.out.println("Conexión establecida con el servidor");
+            System.out.println("Cerrando conexión...");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
